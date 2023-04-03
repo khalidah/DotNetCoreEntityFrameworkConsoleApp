f065ec0bb5721eab23e1458ed0e9a2741bce3b98
khalid Ahmed <khalidcse@gmail.com>
2023-04-03 14:29:14 +0600

    changes

---
+++ ConvertANDInsertImage/DataContext/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ConvertANDInsertImage.Model;
+
+
+namespace ConvertANDInsertImage.DataContext
+{
+    class ApplicationDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"server= ;initial catalog=DB;User Id=sql;Password=Gr;TrustServerCertificate=True");
+             
+        }
+        public DbSet<StudentClass> Studentobj { get; set; }
+        public DbSet<EmployeeClass> Employee { get; set; }
+
+
+    }
+}
